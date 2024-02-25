7c64e2c3f50fba36208b3271d38966cf0c83aa7c
Roman <r92andreevich@gmail.com>
2024-02-25 16:07:39 +0300

    add popup window

--- data.js
+++ data.js
@@ -30,6 +30,12 @@ function initializeSelect2() {
     });
 }
 
+// Функция для закрытия всплывающего окна
+function closePopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'none';
+}
+
 function submitForm() {
     const form = document.getElementById('dataForm');
     const formData = {
@@ -51,14 +57,23 @@ function submitForm() {
         .then(response => response.text())
         .then(data => {
             console.log('Success:', data);
-            alert('Данные успешно отправлены');
+            // Показываем всплывающее окно
+            const popup = document.getElementById('popup');
+            // popup.style.display = 'block';
+            popup.innerText = 'Data added'; // Устанавливаем текст всплывающего окна
+            popup.style.display = 'block';
+            // Таймер для закрытия всплывающего окна через 3 секунды
+            setTimeout(closePopup, 3000);
         })
         .catch((error) => {
             console.error('Error:', error);
-            alert('Ошибка при отправке данных');
+            // Показываем всплывающее окно с сообщением об ошибке
+            const popup = document.getElementById('popup');
+            popup.innerText = 'Error adding data'; // Устанавливаем текст всплывающего окна
+            popup.style.display = 'block';
+            // Таймер для закрытия всплывающего окна через 3 секунды
+            setTimeout(closePopup, 3000);
         });
 }
 
 document.addEventListener('DOMContentLoaded', loadData);
-
-
